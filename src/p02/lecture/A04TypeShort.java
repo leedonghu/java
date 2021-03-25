ec98bb644fe01025822145c7ab719a70aa4c7b78
leedonghu <ldh930517@gmail.com>
2021-03-25 15:14:50 +0900

    variable

---
+++ src/p02/lecture/A04TypeShort.java
@@ -0,0 +1,17 @@
+package p02.lecture;
+
+public class A04TypeShort {
+	public static void main(String[] args) {
+		// short : 2byte (16bite)의 정수 저장
+		
+		//최솟값: -23,768
+		//최댓값: 32,767
+		
+		short shortVar;
+		shortVar = 0;
+		shortVar = -32768;
+		shortVar = 32767;
+//		shortVar = 32768; //not ok
+//		shortVar = -32769; //not ok
+	}
+}
