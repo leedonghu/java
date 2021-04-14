a13e0093175db886f82d12ef1b9d04e3ed04cd6f
leedonghu <ldh930517@gmail.com>
2021-04-14 15:50:32 +0900

    4/14

---
+++ src/p08/textbook/s080301/Audio.java
@@ -0,0 +1,27 @@
+package p08.textbook.s080301;
+
+import p08.textbook.s080205.RemoteControl;
+
+public class Audio implements RemoteControl {
+	private int volume;
+	
+	@Override
+	public void turnOn() {
+		System.out.println("audio를 켭니다.");	
+	}
+	@Override
+	public void turnOff() {
+		System.out.println("audio를 끕니다.");	
+	}
+	@Override
+	public void setVolume(int volume) {
+		if(volume > RemoteControl.MAX_VOLUME) {
+			this.volume = RemoteControl.MAX_VOLUME;
+		}else if(volume<RemoteControl.MIN_VOLUME) {
+			this.volume = RemoteControl.MIN_VOLUME;
+		}else {
+			this.volume = volume;
+		}
+		System.out.println("현재 audio 볼륨: " + this.volume);
+	}
+}
